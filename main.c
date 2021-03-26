00d221cdc8b243abe7139f727445fe53497e0fb0
Jackson Ming Hu <huming2207@gmail.com>
2021-03-27 09:17:13 +1100

    Init

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stm32l0xx_ll_bus.h>
+#include <stm32l0xx_ll_system.h>
+#include <stm32l0xx_ll_pwr.h>
+#include <stm32l0xx_ll_rcc.h>
+#include <stm32l0xx_ll_utils.h>
+
+__attribute__(( long_call, section(".data") )) static void increase_clk_speed()
+{
+    LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+    LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+
+    LL_FLASH_SetLatency(LL_FLASH_LATENCY_1);
+
+    if(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_1)
+    {
+        // Do something here?
+    }
+    LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
+    LL_RCC_HSI_Enable();
+
+    /* Wait till HSI is ready */
+    while(LL_RCC_HSI_IsReady() != 1)
+    {
+
+    }
+    LL_RCC_HSI_SetCalibTrimming(16);
+    LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLL_MUL_4, LL_RCC_PLL_DIV_2);
+    LL_RCC_PLL_Enable();
+
+    /* Wait till PLL is ready */
+    while(LL_RCC_PLL_IsReady() != 1)
+    {
+
+    }
+    LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+    LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+    LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+    LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+
+    /* Wait till System clock is ready */
+    while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+    {
+
+    }
+}
+
+int main()
+{
+    increase_clk_speed();
+
+    LL_Init1msTick(32000000);
+    LL_SetSystemCoreClock(32000000);
+
+    return 0;
+}
